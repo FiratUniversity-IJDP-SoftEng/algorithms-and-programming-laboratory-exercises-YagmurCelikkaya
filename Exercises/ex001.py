c8e47a82fddf851272a5c51d3b4febcc86cc5b25
YagmurCelikkaya <ry.celikkaya@gmail.com>
2025-03-13 12:56:29 +0300

    Update ex001.py

--- Exercises/ex001.py
+++ Exercises/ex001.py
@@ -1,2 +1,8 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543011
+# Your Name and Surname:Yağmur Çelikkaya
+print("Twinkle, twinkle, little star,\n"
+"   How I wonder what you are!\n"
+"\tUp above the world so high,\n"
+"\tLike a diamond in the sky.\n"
+"Twinkle, twinkle, little star,\n"
+"   How I wonder what you are")
