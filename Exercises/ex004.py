5dab09b57537f6219d598d857801d191ab5be1cb
YagmurCelikkaya <ry.celikkaya@gmail.com>
2025-03-13 12:58:23 +0300

    Update ex004.py

--- Exercises/ex004.py
+++ Exercises/ex004.py
@@ -1,2 +1,9 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543011
+# Your Name and Surname:Yağmur Çelikkaya
+import math
+radius = input("Please enter the rradius of the circle: ")
+r = (float)(radius)
+area = (math.pi) * r * r 
+print("Input the radius of the circle:", radius)
+print(f"the area of the circle with radius {radius} is: {area:.2f}")
+
